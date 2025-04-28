590c4d55cb59fef5ebd1bae6706b7b9f5f7c3395
Ubuntu <azureuser@superbench-nc48.om4nu1raaxkerkldm4j2ojb3xd.ax.internal.cloudapp.net>
2025-04-28 17:15:22 +0000

    Update LICENSE

--- omnigirl/harness/scripts/Eval_Claude_AgentlessX.sh
+++ omnigirl/harness/scripts/Eval_Claude_AgentlessX.sh
@@ -1,1 +1,1 @@
-python run_evaluation.py --dataset_name benchmark/OmniGIRL.json --predictions_path patch/full_set/Claude-3.5-Sonnet-2024-06-25/agentlessX/patch.jsonl --max_workers 20 --run_id eval_claude_agentlessX
+python run_evaluation.py --dataset_name benchmark/OmniGIRL.json --predictions_path patch/full_set/Qwen-72b/agentlessX/patch.jsonl --max_workers 20 --run_id eval_claude_agentlessX
